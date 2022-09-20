522723d268aa622b11ad1e0cc1433ea046ea32aa
skprpi <60091575+skprpi@users.noreply.github.com>
2022-09-20 20:34:30 +0300

    Update 2_refactor.py

--- 4/2_refactor.py
+++ 4/2_refactor.py
@@ -1,67 +1,52 @@
 class Person:
-
-    def __init__(self, floor):
-        self.floor = floor
-
-    def put_button(self, lift):
-        lift.add_order(self.floor)
+    def __init__(self, current_floor, next_floor):
+        self.current_floor = current_floor
+        self.next_floor = next_floor
 
 
 class Lift:
-    def __init__(self, cnt_floors):
-        self.state = UpLift(cnt_floors, 1)
-    
+    def __init__(self, cnt_floors, current_floor):
+        self.state = LiftUp(cnt_floors, current_floor)
+
+    def add_order(self, a, b, person):
+        return self.state.add_order(a, b, person)
+
     def go_next(self):
-        self.state = self.state.go_next()
-
-    def add_order(self, n):
-        self.state.add_order(n)
-
-    def empty(self):
-        self.state.empty()
+        return self.state.go_next()
 
 
-class UpLift:
+class LiftUp:
     def __init__(self, cnt_floors, current_floor):
         self.cnt_flors = cnt_floors
         self.current_floor = current_floor
-        self.floor_person = dict()
-        self.down_lift = DownLift()
-        self.order_list = set()
+        self.floor_take_person = dict()
+        self.floor_out_person = dict()
+        self.persons = []
 
     # заказываем лифт
-    def add_order(self, n):
-        if n >= self.cnt_flors:
-            self.order_list.add(n)
-            return
-        self.down_lift.order_list.add(n)
-
-    def empty(self):
-        return len(self.persons) == 0
+    def add_order(self, a, b, person):
+        if self.current_floor > a > b:
+            self.floor_take_person[a].append(person)
+            self.floor_out_person[b].append(person)
+            return "ok"
+        raise "Unvalliable to take a person"
 
     # лифт едет на следующий этаж
-    def go_next(self, avaliable_person):
-        if self.current_floor in self.order_list: # забираем кого нужно забрать
-            self.__add_person(avaliable_person)
-            self.order_list.remove(self.current_floor) # убираем из очереди
-        del self.floor_person[self.current_floor] # вызадили всех кого надо
-        if self.cnt_flors == self.current_floor:
-            return self.down_lift # начинаем опискаться
-        self.current_floor += 1
-        return self
-
-    def __add_person(self, person):
-        if person.floor >= self.current_floor:
-            self.floor_person[person.floor].append(person)
-            return
-        self.down_lift.floor_person[person.floor].append(person)
+    def go_next(self):
+        if self.current_floor in self.floor_take_person: # забираем кого нужно забрать
+            self.persons += self.floor_take_person[self.current_floor]
+        if self.current_floor in self.floor_out_person: # вызадили всех кого надо
+            self.persons = list(set(self.persons).difference(set(self.floor_out_person[self.current_floor])))
 
 
 class DownLift:
+    pass
     # as a up lift
-        
+
 
 class LiftManager:
-
-    def __init__(self, lifts: List[Lift])
+    def __init__(self, lifts: List[Lift]):
         self.lifts = lifts
+
+    def manage(self, a, b, person):
+        pass
